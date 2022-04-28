0844ac5a649e27b3487a2c5c782ec0f143a3101a
Александр Стрюков <sanyajump16@gmail.com>
2022-04-28 23:09:49 +0300

    invalid value for sending data

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    public void shouldSendRequestBody () {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Test") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("Automatization"))
+        ;
+    }
+}
